3dfece58b9eff3f6b5d73d65f02b3e587b5e450b
Marcel Widmer <marzelwidmer@gmail.com>
2017-06-28 21:14:28 +0200

    add some docker stuff

--- src/main/java/ch/keepcalm/web/ApiGatewayApplication.java
+++ src/main/java/ch/keepcalm/web/ApiGatewayApplication.java
@@ -3,10 +3,18 @@ package ch.keepcalm.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableZuulProxy
 public class ApiGatewayApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
